8f5ed896f771491b0a34a20737abe275dca2facb
AriellaYamada <ariella.y.b@hotmail.com>
2015-10-01 23:24:21 -0300

    Definindo variaveis, analisando tipos

--- src/lru.js
+++ src/lru.js
@@ -1,1 +1,18 @@
+//Defining variables
+var nest;
+var chickens;
+
+function initializingVariables() {
+    nest = new Array(6);
+    for (var i = 0; i < 6; i++) {
+        nest[i] = {
+            bitR: 0,
+            chicken: 0,
+            access: [0, 0, 0, 0, 0, 0]
+        };
+    }
+    chickens = ["galinha1.png", "galinha3.png", "galinha3.png", "galinha4.png", "galinha5.png", "galinha6.png"];
+}
+
+
 
