d7318b2d52dec31c131695a7cfb4350f9e197acf
AriellaYamada <ariella.y.b@hotmail.com>
2015-10-05 08:14:45 -0300

    Mostrando bits de acesso

--- src/lru.js
+++ src/lru.js
@@ -15,13 +15,17 @@ function initializingVariables() {
             auxAccess: ""
         };
     }
-    chickens = ["images/Galinha1.png", "images/Galinha3.png", "images/Galinha3.png", "images/Galinha4.png", "galinha5.png", "galinha6.png"];
+    chickens = ["images/Galinha1.png", "images/Galinha3.png", "images/Galinha3.png",
+        "images/Galinha4.png", "imagesGalinha5.png", "galinha6.png", "imagesGalinha7.png",
+        "imagesGalinha8.png", "imagesGalinha9.png", "imagesGalinha10.png", "imagesGalinha11.png",
+        "imagesGalinha12.png", "imagesGalinha12.png", "imagesGalinha13.png", "imagesGalinha14.png",
+        "imagesGalinha15.png", "imagesGalinha16.png"];
     //interval = setInterval(interruption(), 300);
+    updateScenario();
 }
 
 function accessingNest(nNest) {
     nest[nNest].bitR = 1;
-    console.log(nest[nNest].bitR);
     var feeder = "feeder" + nNest;
     document.getElementById(feeder).src = "images/bit1.png";
 }
@@ -31,17 +35,22 @@ function updateScenario() {
         var n = "nest" + i;
         var f = "feeder" + i;
         document.getElementById(f).src = "images/bit0.png";
-        console.log("imagem: " + nest[i].chicken);
         document.getElementById(n).src = chickens[nest[i].chicken];
+        for (var j = 0; j < 4; j++) {
+            var image = "images/access" + nest[i].access[j] + ".png";
+            var bit = "c" + i + "access" + j;
+            console.log("\nteste3 acesso: " + bit + " imagem: "+ image);
+            document.getElementById(bit).src = image;
+        }
     }
+    console.log("teste4");
 }
 
 function interruption() {
-    console.log("interruption");
     for (var i = 0; i < 6; i++) {
         nest[i].access.pop();
         nest[i].access.unshift(nest[i].bitR);
-        nest[i].chicken += nest[i].bitR;
+        nest[i].chicken += getAccess(i);
         if (nest[i].bitR == 0) {
             if(nest[i].chicken > 0)
                 nest[i].chicken--;
@@ -54,8 +63,11 @@ function interruption() {
 }
 
 function getAccess(nNest) {
-    for(var i = 0; i < 4; i++)
+    nest[nNest].auxAccess = 0;
+    for(var i = 0; i < 4; i++) {
+        nest[nNest].auxAccess *= 2;
         nest[nNest].auxAccess += nest[nNest].access[i];
+    }
     return nest[nNest].auxAccess;
 }
 
